99fc57c1305637e4e47ee1d71e32d61eb578f78d
rrbalat <matej.balat@reroot.hr>
2021-01-20 23:59:58 +0100

    Server template

--- source/server.ts
+++ source/server.ts
@@ -2,34 +2,53 @@ import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import logging from './config/logging';
-import config from './config/logging';
+import config from './config/config';
+import sampleRoutes from './routes/sample';
 
 const NAMESPACE = 'Server';
 const router = express();
 
-/** Logging the request */
+/** Loging the request */
 router.use((req, res, next) => {
-    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}, IP - [${req.socket.remoteAddress}]]`);
+    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
 
-    res.on(`finish`, () => {
-        logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}, IP - [${req.socket.remoteAddress}]], STATUS - [${res.statusCode}]`);
+    res.on('finish', () => {
+        logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${res.statusCode}]`);
     });
+
+    next();
 });
 
 /** Parse the request */
 router.use(bodyParser.urlencoded({ extended: false}));
 router.use(bodyParser.json());
 
-/** Rules of API */
+/** Rules for API */
 router.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-Width, Content-Type, Accept, Authorization');
+    res.header('Access-Controll-Allow_Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorized');
     
-    if(req.method == `OPTIONS`)
+    if(req.method == 'OPTIONS')
     {
-        res.header('Access-Control-Allow-Methods', `GET PATCH DELETE POST PUT`);
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
         return res.status(200).json({});
     }
+
+    next();
 });
 
 /** Routes */
+router.use('/sample', sampleRoutes);
+
+/** Error Handling */
+router.use((req, res, next) => {
+    const error = new Error('not found');
+
+    return res.status(404).json({
+        message: error.message
+    });
+});
+
+/** Create the server */
+const httpServer = http.createServer(router);
+httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server running on ${config.server.hostname}: ${config.server.port}`));
